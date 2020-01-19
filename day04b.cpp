1fdb6c05a066881d2a363c6a37824df651c3e889
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-01-19 11:26:46 -0500

    Day 4b completed

---
+++ day04b.cpp
@@ -0,0 +1,81 @@
+//--- Day 4: Secure Container ---
+/*
+You arrive at the Venus fuel depot only to discover it's protected by a password. The Elves had written the password on a sticky note, but someone threw it out.
+
+However, they do remember a few key facts about the password:
+
+    It is a six-digit number.
+    The value is within the range given in your puzzle input.
+    Two adjacent digits are the same (like 22 in 122345).
+    Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
+*/
+
+/*--- Part Two ---
+
+An Elf just remembered one more important detail: the two adjacent matching digits are not part of a larger group of matching digits.
+
+Given this additional criterion, but still ignoring the range rule, the following are now true:
+
+    112233 meets these criteria because the digits never decrease and all repeated digits are exactly two digits long.
+    123444 no longer meets the criteria (the repeated 44 is part of a larger group of 444).
+    111122 meets the criteria (even though 1 is repeated more than twice, it still contains a double 22).
+
+How many different passwords within the range given in your puzzle input meet all of the criteria?
+*/
+
+#include <iostream>
+#include <string>
+int PASSWORDMIN = 254032;
+int PASSWORDMAX = 789860;
+
+using namespace std;
+
+//Boolean xor
+bool bxor(bool p, bool q)
+{
+	return(p && !q || !p && q);
+}
+
+
+bool passwordIsValid(int a, int b, int c, int d, int e, int f)
+{
+	int builtPassword = a*1e5 + b*1e4 + c*1e3 + d*1e2 + e*1e1 + f;
+	bool passwordOK = 
+		(a==b && b!=c || a!=b&&b==c&&c!=d || b!=c&&c==d&&d!=e || c!=d&&d==e&e!=f || d!=e&&e==f) &&
+		builtPassword >= PASSWORDMIN &&
+		builtPassword <= PASSWORDMAX;
+	// if(passwordOK)
+		// cout<<builtPassword<<"\n";
+	// else
+		// cout<<"REJECTED "<<builtPassword<<"\n";
+	// return passwordOK;
+}
+
+int main()
+{
+	int validPasswords = 0;
+	printf("Running\n");
+	
+	for(int a=0; a<=9; a++)
+	{
+	for(int b=a; b<=9; b++)
+	{
+	for(int c=b; c<=9; c++)
+	{
+	for(int d=c; d<=9; d++)
+	{
+	for(int e=d; e<=9; e++)
+	{
+	for(int f=e; f<=9; f++)
+	{
+		validPasswords += passwordIsValid(a,b,c,d,e,f) ? 1 : 0;
+	}
+	}
+	}
+	}
+	}
+	}
+	
+	cout << validPasswords << "\n";
+	return 0;
+}
