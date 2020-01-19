a832f628161378613b6d70c5f193d1417c023106
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-01-18 21:24:07 -0500

    Day 4a completed

--- day04a.cpp
+++ day04a.cpp
@@ -11,12 +11,45 @@ However, they do remember a few key facts about the password:
 */
 #include <iostream>
 #include <string>
+int PASSWORDMIN = 254032;
+int PASSWORDMAX = 789860;
 
 using namespace std;
 
+bool passwordIsValid(int a, int b, int c, int d, int e, int f)
+{
+	int builtPassword = a*1e5 + b*1e4 + c*1e3 + d*1e2 + e*1e1 + f;
+	bool passwordOK = (a==b || b==c || c==d || d==e || e==f) && builtPassword >= PASSWORDMIN && builtPassword <= PASSWORDMAX;
+	// if(passwordOK)
+		// cout<<builtPassword<<"\n";
+	// else
+		// cout<<"REJECTED "<<builtPassword<<"\n";
+	return passwordOK;
+}
+
 int main()
 {
+	int validPasswords = 0;
 	printf("Running\n");
-
+	for(int a=0; a<=9; a++)
+	{
+	for(int b=a; b<=9; b++)
+	{
+	for(int c=b; c<=9; c++)
+	{
+	for(int d=c; d<=9; d++)
+	{
+	for(int e=d; e<=9; e++)
+	{
+	for(int f=e; f<=9; f++)
+	{
+		validPasswords += passwordIsValid(a,b,c,d,e,f) ? 1 : 0;
+	}
+	}
+	}
+	}
+	}
+	}
+	cout << validPasswords << "\n";
 	return 0;
 }
