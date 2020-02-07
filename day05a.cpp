eb827131c0e852e894cc529fdcca6db3749d014a
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-02-07 09:21:19 -0500

    More work on opcode reading. Starting abstraction for memory reads/writes in different modes

--- day05a.cpp
+++ day05a.cpp
@@ -4,6 +4,19 @@
 #define DEBUG 0
 int MEMSIZE = 117;
 int MAXOPCODEPARAMS = 4;
+int OPCODEPARAMS [5] = {-1,3,3,2,2};
+//[NONE,add,multiple,store,print]
+
+//OpCode names
+const int ADD = 1;
+const int MUL = 2;
+const int STOR = 3;
+const int PRNT = 4;
+const int EXIT = 99;
+
+//Address modes
+const int POSITION = 0;
+const int IMMEDIATE = 1;
 
 using namespace std;
 
@@ -13,7 +26,15 @@ struct OpCode
 	int* parameters = new int[MAXOPCODEPARAMS];
 	int* paramModes = new int[MAXOPCODEPARAMS];
 	
-	
+	void clearParams()
+	{
+		for(int i=0; i<MAXOPCODEPARAMS; i++)
+		{
+			paramModes[i] = -1;
+			parameters[i] = -1;
+		}
+		
+	}
 };
 
 void initializeMemory(int* memory)
@@ -44,36 +65,73 @@ int extractDecimalDigit(int number, int digit)
 }
 
 //Reads an opcode starting from the current program counter
+//OPCODE format: ABCDE, C, B, A, [start of next opcode].
+//               |||^^ Two digit operation
+//               ^^^   Parameter modes. May be more or less of them depending on the operation. Leftmost 0-mode (position) may be omitted in the spec
 void readOpCode(int* memory, OpCode* opcode, int pc)
 {
 	int protoOpCode = memory[pc]; //This includes the operation but also parameter modes
+	opcode->clearParams();
+	opcode->operation = extractDecimalDigit(protoOpCode,1)*10 + extractDecimalDigit(protoOpCode,0);
 	
+	if(opcode->operation == EXIT) //99 is our stop opcode
+		exit(0);
+	
+	for(int opcodeParam = 0; opcodeParam < OPCODEPARAMS[opcode->operation]; opcodeParam++)
+	{
+		opcode->paramModes[opcodeParam]=extractDecimalDigit(protoOpCode,opcodeParam+2);
+		opcode->parameters[opcodeParam]=memory[pc+opcodeParam+1];
+	}
+	
+}
+
+int readMem(int* memory, int param, const int mode)
+{
+	switch(mode)
+	{
+		case POSITION:
+			return memory[param];
+		case IMMEDIATE:
+			return param;
+		default:
+			cout << "INVALID MEMORY MODE: " << mode << "\n";
+			exit(-1);
+	}
+}
+
+void writeMem(int* memory, const int data, const int param, const int mode)
+{
+	switch(mode)
+	{
+		case POSITION:
+			memory[param] = data;
+		case IMMEDIATE:
+			cout << "NONSENSICAL MEMORY MODE: " << mode << "\n";
+		default:
+			cout << "INVALID MEMORY MODE: " << mode << "\n";
+			exit(-1);
+	}
 }
 
 void intCodeInterpreter(int* memory)
 {
 	int pc = 0; //The program counter
-	int operation;
-	int sourceAddr1; 
-	int sourceAddr2;
-	int targetAddr;
+	OpCode* opcode = new OpCode();
 	while(true)
 	{
-		operation = memory[pc];
-		sourceAddr1 = memory[pc+1];
-		sourceAddr2 = memory[pc+2];
-		targetAddr= memory[pc+3];
-		//printf("%d, %d, %d, %d\n", operation, sourceAddr1, sourceAddr2, targetAddr);
-		switch(operation)
+		readOpCode(memory, opcode, pc);
+		switch(opcode->operation)
 		{
-			case 1:
-				memory[targetAddr] = memory[sourceAddr1] + memory[sourceAddr2];
+			case ADD:
+				//memory[targetAddr] = memory[sourceAddr1] + memory[sourceAddr2];
 				break;
-			case 2:
-				memory[targetAddr] = memory[sourceAddr1] * memory[sourceAddr2];
+			case MUL:
+				//memory[targetAddr] = memory[sourceAddr1] * memory[sourceAddr2];
 				break;
-			case 99:
-				return;
+			case STOR:
+				break;
+			case PRNT:
+				break;
 			default:
 				cout << "INVALID OPCODE ERROR\n";
 				exit(-1);
