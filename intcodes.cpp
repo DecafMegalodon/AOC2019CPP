8af369d089d0aaeb8451539d2ad5ce3fda342797
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-04-07 09:15:48 -0400

    Factoring out common intcode code

---
+++ intcodes.cpp
@@ -0,0 +1,127 @@
+/*
+This file contains some functions common to all intcode challenges.
+Some functions may not be included because they are percived as
+potentially breaking to future or past challenges if they are updated
+*/
+
+#include <iostream>
+#include <cmath>
+#include "intcodes.h"
+
+using namespace std;
+//Read an initial memory state from stdin. 
+void initializeMemory(int* memory, int size)
+{
+	int curRead;
+	int addr = 0;
+	char seperator;
+	do
+	{
+		seperator='\0';
+		scanf("%d%c",&curRead, &seperator);
+		memory[addr] = curRead;
+		if(addr >= size)
+		{
+			cout << "Memory overflow. Please download more RAM.\n";
+			exit(-1);
+		}
+		addr++;
+	}
+	while (seperator == ',');
+	for(; addr < size; ++addr)
+	{
+		memory[addr] = 0;
+	}
+}
+
+//Extracts a single decimal digit from number, in the digit-th spot. Right to left, starting at 0
+//If there is no such decimal digit it will be treated as being padded with zeros
+int extractDecimalDigit(int number, int digit)
+{
+	int trimmed = number % (int)pow(10, digit+1); //Floating point error???
+	return (int)(trimmed / pow(10, digit));
+}
+
+//Reads an opcode starting from the current program counter
+//OPCODE format: ABCDE, C, B, A, [start of next opcode].
+//               |||^^ Two digit operation
+//               ^^^   Parameter modes. May be more or less of them depending on the operation. Leftmost 0-mode (position) may be omitted in the spec
+//C B A are the parameters for the opcode and match with the corresponding mode packed with the opcode
+bool readOpCode(int* memory, OpCode* opcode, int pc)
+{
+	int protoOpCode = memory[pc]; //This includes the operation but also parameter modes
+	opcode->clearParams();
+	opcode->operation = extractDecimalDigit(protoOpCode,1)*10 + extractDecimalDigit(protoOpCode,0);
+	
+	if(opcode->operation == EXIT) //EXIT is our halt opcode
+		return false;
+	
+	if(opcode -> operation == 0 || opcode->operation > 8) //Invalid opcode detected
+		return false;
+	
+	for(int opcodeParam = 0; opcodeParam < OPCODEPARAMS[opcode->operation]; opcodeParam++)
+	{
+		opcode->paramModes[opcodeParam]=extractDecimalDigit(protoOpCode,opcodeParam+2);
+		opcode->parameters[opcodeParam]=memory[pc+opcodeParam+1];
+	}
+	return true;
+}
+
+void printRAM(int* memory, int size)
+{
+	for(int addr = 0; addr < size; addr+= 8)
+	{
+		printf("[%5d]", addr);
+		for(int offset = 0; offset < 8; offset++)
+		{
+			printf("%8d ", memory[addr+offset]);
+		}
+		cout << endl;
+	}
+	cout << "\n";
+}
+
+int readMem(int* memory, const int param, const int mode)
+{
+	switch(mode)
+	{
+		case POSITION:
+			if(param < MEMSIZE)
+				return memory[param];
+			else
+			{
+				cout << "Memory overflow. Please download more RAM.\n";
+				exit(-1);
+			}
+		case IMMEDIATE:
+			return param;
+		default:
+			cout << "INVALID MEMORY MODE: " << mode << "\n";
+			exit(-1);
+	}
+}
+
+void writeMem(int* memory, const int data, const int param, const int mode)
+{
+	switch(mode)
+	{
+		case POSITION:
+			if(param < MEMSIZE)
+			{
+				memory[param] = data;
+				//cout << data << "Write->" << param << endl;
+			}
+			else
+			{
+				cout << "Memory overflow. Please download more RAM.\n";
+				exit(-1);
+			}
+			break;
+		case IMMEDIATE:
+			cout << "NONSENSICAL MEMORY MODE: " << mode << "\n";
+			exit(-1);
+		default:
+			cout << "INVALID MEMORY MODE: " << mode << "\n";
+			exit(-1);
+	}
+}
--- intcodes.h
+++ intcodes.h
@@ -1,7 +1,9 @@
+#ifndef INTCODES_H
+#define INTCODES_H
 //OpCode names
 const int ADD = 1;
 const int MUL = 2;
-const int PRMPT = 3;
+const int STOR = 3;
 const int PRNT = 4;
 const int JIT = 5;
 const int JIF = 6;
@@ -12,3 +14,50 @@ const int EXIT = 99;
 //Address modes
 const int POSITION = 0;
 const int IMMEDIATE = 1;
+
+const int MAXOPCODEPARAMS = 4;
+
+const int OPCODEPARAMS [] = {-1,3,3,1,1,2,2,3,3};
+static const char* OPCODENAMES[] = {"NONE", "ADD", "MULT", "STOR", "PRNT", "JIT", "JIF", "LT", "EQ"};
+
+const int MEMSIZE = 1024;
+//[NONE,add,multiple,store,print, jumpiftrue, jumpiffalse, lessthan, equal]
+
+struct OpCode
+{
+	int operation;
+	int* parameters = new int[MAXOPCODEPARAMS];
+	int* paramModes = new int[MAXOPCODEPARAMS];
+	
+	void clearParams()
+	{
+		for(int i=0; i<MAXOPCODEPARAMS; i++)
+		{
+			paramModes[i] = -1;
+			parameters[i] = -1;
+		}
+		
+	}
+};
+
+void initializeMemory(int* memory, int size);
+
+//Extracts a single decimal digit from number, in the digit-th spot. Right to left, starting at 0
+//If there is no such decimal digit it will be treated as being padded with zeros
+int extractDecimalDigit(int number, int digit);
+
+//Reads an opcode starting from the current program counter
+//OPCODE format: ABCDE, C, B, A, [start of next opcode].
+//               |||^^ Two digit operation
+//               ^^^   Parameter modes. May be more or less of them depending on the operation. Leftmost 0-mode (position) may be omitted in the spec
+//C B A are the parameters for the opcode and match with the corresponding mode packed with the opcode
+bool readOpCode(int* memory, OpCode* opcode, int pc);
+
+//Prints RAM in slightly more human readable chunks of integers
+void printRAM(int* memory, int size);
+
+int readMem(int* memory, const int param, const int mode);
+
+void writeMem(int* memory, const int data, const int param, const int mode);
+
+#endif
