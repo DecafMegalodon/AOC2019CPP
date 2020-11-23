ac0ad276d387d1141c31cea4e070b0aec0bcc257
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-11-23 18:00:42 -0500

    Stealing my automated tester from 2017 and adapting it

---
+++ test_all.py
@@ -0,0 +1,47 @@
+#  Automated test for AOC 2017 based on my personal puzzle inputs
+import fileinput
+import subprocess
+import time
+
+testsPassed = 0
+testsFailed = 0
+testsSlow = 0
+results = [line.strip() for line in fileinput.input(files=("inputs/results.txt"))]
+
+
+for day in range(1,1+1):
+    paddedDay = ('0' if day < 10 
+                else '') + str(day)
+    dayInput = open("inputs/day" + paddedDay + ".txt")
+    for part in ('a', 'b'):
+        if day == 25 and part == 'b': #  No part b on on 25th
+            break
+        progName = ''.join(['day', paddedDay, part, '.py'])
+        
+        startTime = time.time()
+        subProc = subprocess.run(args=["python3", progName],
+                                stdin=dayInput, capture_output=True)
+        stopTime = time.time()
+        timeTaken = round(stopTime - startTime, 4)
+        
+        output = str(subProc.stdout.decode('utf-8')).strip()
+        expected = results[(day*2) + (-2 if part == 'a' else -1)]
+        testCorrect = (output == expected)
+        
+        result = "OK" if testCorrect else "FAIL"
+        if testCorrect and timeTaken > 15:
+            result = "SLOW"
+            
+        print("D" + paddedDay + part, result, timeTaken, "seconds")
+
+        if testCorrect:
+            testsPassed += 1
+        else:
+            testsFailed += 1
+
+        if timeTaken >= 15:
+            testsSlow += 1
+
+        dayInput.seek(0,0)
+
+print("Test results: %d passed, %d failed, %d slow" % (testsPassed, testsFailed, testsSlow))
