acd714589255cd316377d39e4aea8d2f0afd844e
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-11-27 18:30:11 -0500

    Added compilation to cTest2

--- cTest2.py
+++ cTest2.py
@@ -1,5 +1,7 @@
 '''
-For cTest2:
+cTest2 is intended to be a tool to automate testing workflows for groups of
+simple programs that use I/O from standard in and out respectively.
+
 
 in /tests/:
 	Will attempt to compile and run tests in .json files.
@@ -18,6 +20,9 @@ in /tests/:
 
 import os
 import json
+import subprocess
+SMALL_LINE = "--------------------"
+BIG_LINE = SMALL_LINE * 3
 
 def runSingleTest(testFilePath):
     test_file = open(testFilePath, 'r')
@@ -25,7 +30,16 @@ def runSingleTest(testFilePath):
     test_file.close()
     test = json.loads(test_JSON)
     
-    print(test)
+    print(BIG_LINE)
+    print(testFilePath, test['testIdentifier'])
+    print(BIG_LINE)
+    try:
+        test_input_file = open(test['testInput'])
+        build_opts = ['g++', '-O3', '-o', 'testCompiled'] + test['compilationFiles']        
+        compilation_process = subprocess.run(build_opts, stdin=test_input_file, capture_output=True)
+        print(compilation_process.stderr.decode('utf-8'))
+    except Exception as e:
+        print(f'There was a problem running this test. The reason given was: {e}')
 
 def runTestsDir(directory):
     for testDescriptor in os.listdir(directory):
@@ -35,7 +49,6 @@ def runTestsDir(directory):
             runTestsDir(fullPath)
         elif testDescriptor.endswith(".json"):
             runSingleTest(fullPath)
-            print("test")
         #  else ignore non-test files
             
 
