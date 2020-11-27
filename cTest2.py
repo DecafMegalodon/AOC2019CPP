d510bb88d1d3e1455c0bad680fab8f913f156b44
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-11-27 18:40:20 -0500

    Fix recursing into nested directories

--- cTest2.py
+++ cTest2.py
@@ -33,20 +33,23 @@ def runSingleTest(testFilePath):
     print(BIG_LINE)
     print(testFilePath, test['testIdentifier'])
     print(BIG_LINE)
+    
     try:
         test_input_file = open(test['testInput'])
         build_opts = ['g++', '-O3', '-o', 'testCompiled'] + test['compilationFiles']        
-        compilation_process = subprocess.run(build_opts, stdin=test_input_file, capture_output=True)
-        print(compilation_process.stderr.decode('utf-8'))
+        compilation_process = subprocess.run(build_opts, stdin=test_input_file, capture_output=True)    
     except Exception as e:
-        print(f'There was a problem running this test. The reason given was: {e}')
+        print(f'There was a problem with this test configuration. The reason given was: {e}')
+    
+    
+    
 
 def runTestsDir(directory):
     for testDescriptor in os.listdir(directory):
         fullPath = directory + testDescriptor
         if os.path.isdir(fullPath):
             print(f"Recursively looking for tests in {fullPath}")
-            runTestsDir(fullPath)
+            runTestsDir(fullPath+'/')
         elif testDescriptor.endswith(".json"):
             runSingleTest(fullPath)
         #  else ignore non-test files
