f42ce1ac27a77534a228e46504ec3c8d0c0f9fb0
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-11-28 15:45:50 -0500

    Standardize to snake_case and propose new test file format

--- cTest2.py
+++ cTest2.py
@@ -3,19 +3,33 @@ cTest2 is intended to be a tool to automate testing workflows for groups of
 simple programs that use I/O from standard in and out respectively.
 
 
-in /tests/:
+in ./tests/:
 	Will attempt to compile and run tests in .json files.
     File paths should be relative to PWD
-    Files should be in JSON format with the following fields.:
-		{
-		  "testIdentifier": "test name"
-		  "compilationFiles": [
-			"file1.cpp",
-			"file2.cpp" ...
-		  ],
-		  "testInput": "filePath",
-		  "testOutput": "filePath"
-		}
+    Files should be in JSON format with the following structure:
+    {
+      "testFamilyName": "name. Optional if only one test",
+      "compilationFiles": [
+        "file1.cpp",
+        "file2.cpp",
+        ...
+      ],
+      "tests": [
+        {
+          "testIdentifier": "testName",
+          "testInput": "filePath",
+          "testOutputType": "simple or file",
+          "testOutput": "filePath or value"
+        },
+        {
+          "testIdentifier": "testName",
+          "testInput": "filePath",
+          "testOutputType": "simple or file",
+          "testOutput": "filePath or value"
+        },
+        ...
+      ]
+    }
 '''
 
 import os
@@ -26,8 +40,8 @@ SMALL_LINE = "--------------------"
 BIG_LINE = SMALL_LINE * 3
 
 #  Runs a single test. -1 for failures, 0 for successes
-def runSingleTest(testFilePath):
-    test_file = open(testFilePath, 'r')
+def run_test_file(test_file_path):
+    test_file = open(test_file_path, 'r')
     test_JSON = test_file.read()
     test_file.close()
     test = json.loads(test_JSON)
@@ -44,14 +58,15 @@ def runSingleTest(testFilePath):
         
     except Exception as e:
         print(BIG_LINE)
-        print(testFilePath, test['testIdentifier'])        
+        print(test_file_path, test['testIdentifier'])        
         print(Fore.RED, f'[ERROR] There was a problem with this test configuration. The reason given was: {e}', Style.RESET_ALL)
         print(BIG_LINE)
         return -1
-        
+
+
     if compilation_process.returncode != 0: #  compilation failed
         print(BIG_LINE)
-        print(testFilePath, test['testIdentifier'])        
+        print(test_file_path, test['testIdentifier'])        
         print(Fore.RED, '[FAIL] This test did not successfully compile. The given message was:', Style.RESET_ALL)
         print(compilation_process.stderr.decode('utf-8'))
         print(BIG_LINE)
@@ -63,11 +78,11 @@ def runSingleTest(testFilePath):
     test_output = test_execution_process.stdout.decode('utf-8').strip()
     
     if test_output == expected_output:
-        print(Fore.GREEN, '[PASS]', Style.RESET_ALL, testFilePath, test['testIdentifier'])
+        print(Fore.GREEN, '[PASS]', Style.RESET_ALL, test_file_path, test['testIdentifier'])
         return 0
     else:
         print(BIG_LINE)
-        print(testFilePath, test['testIdentifier'])        
+        print(test_file_path, test['testIdentifier'])        
         print(SMALL_LINE)
         print(Fore.RED, '[FAIL] The program did not produce the expected output', Style.RESET_ALL)
         print("Expected:\n", expected_output)
@@ -76,16 +91,16 @@ def runSingleTest(testFilePath):
         print(BIG_LINE, '\n')
     
 
-def runTestsDir(directory):
-    for testDescriptor in os.listdir(directory):
-        fullPath = directory + testDescriptor
-        if os.path.isdir(fullPath):
-            #  print(f"Recursively looking for tests in {fullPath}")
-            runTestsDir(fullPath+'/')
-        elif testDescriptor.endswith(".json"):
-            runSingleTest(fullPath)
+def run_tests_dir(directory):
+    for test_descriptor in os.listdir(directory):
+        full_path = directory + test_descriptor
+        if os.path.isdir(full_path):
+            #  print(f"Recursively looking for tests in {full_path}")
+            run_tests_dir(full_path+'/')
+        elif test_descriptor.endswith(".json"):
+            run_test_file(full_path)
         #  else ignore non-test files
             
 
-runTestsDir("./tests/")
+run_tests_dir("./tests/")
 
